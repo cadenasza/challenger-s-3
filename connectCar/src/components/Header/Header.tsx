6af2d5507946e80c7bd57a381ece30825403cd76
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-13 16:36:23 -0300

    instalando styled e colocando header

---
+++ connectCar/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+
+export default function Headerr() {
+  return (
+    <Header>
+      <h1>API - GITHUB</h1>
+      <Menu />
+    </Header>
+  );
+}
--- connectCar/src/main.tsx
+++ connectCar/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import App from "./App.tsx";
+import Home from "./routes/Home/index.tsx";
+import NotFound from "./routes/NotFound/index.tsx";
+import Usuarios from "./routes/Usuarios/index.tsx";
+import Produtos from "./routes/Produtos/index.tsx";
+import EditarProdutos from "./routes/EditarProdutos/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/usuarios", element: <Usuarios /> },
+      { path: "/produtos", element: <Produtos /> },
+      { path: "/editar/produtos/:id", element: <EditarProdutos /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </StrictMode>
+);
