629ca96a51c2df998db7b42c26d94842c2d118b9
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-13 17:07:45 -0300

    adicionando fonte mansalva

--- connectCar/src/components/Header/Header.tsx
+++ connectCar/src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
 import { Header } from '../../style/styled';
 
 export default function Cabecalho() {
   return (
     <Header>
-      <h1>ConnectCar</h1>
+      <div className="logo-team">
+        <p className="porto" id="porto-seguro">ConectCar</p>
+      </div>
+      <div className="header-content-container">
+        <Link to="/"><p>Home</p></Link>
+        <Link to="/diagnosticar/veiculo"><p>Diagnosticar Veículo</p></Link>
+        <Link to="/nos"><p>Sobre Nós</p></Link>
+        <Link to="/projeto"><p>Sobre o Projeto</p></Link>
+      </div>
     </Header>
   );
 }
--- connectCar/src/style/styled.ts
+++ connectCar/src/style/styled.ts
@@ -1,20 +1,73 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-  background-color: black;
-  padding: 20px;
-  text-align: center;
-  color: white;
-  font-size: 1.5em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 15%;
+  width: 100vw;
+  background-color: #333;
+
+  a {
+    position: relative;
+    text-decoration: none;
+    color: whitesmoke;
+    font-size: 2.3rem;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+  }
+
+  a::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 4px;
+    border-radius: 4px;
+    background-color: whitesmoke;
+    bottom: 0;
+    left: 0;
+    transform-origin: right;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  a:hover {
+    font-size: 2.4rem;
+    transition: 0.3s;
+  }
+
+  a:hover::before {
+    transform-origin: left;
+    transform: scaleX(1);
+  }
+
+  .logo-team {
+    display: flex;
+  }
+
+  .porto {
+    display: flex;
+    font-family: "Mansalva", sans-serif;
+    font-size: 4rem;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  #porto-porto {
+    color: #2762ca;
+  }
+
+  #porto-seguro {
+    color: whitesmoke;
+  }
+
+  .header-content-container {
+    display: flex;
+    gap: 3vw;
+  }
 `;
 
-// Estilizando o Footer
 export const Footer = styled.footer`
   background-color: #333;
-  padding: 10px;
-  text-align: center;
-  color: white;
-  position: fixed;
-  width: 100%;
-  bottom: 0;
 `;
