edd439012d436ae340c6967773d30fc99d4ac2e3
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-15 18:23:24 -0300

    consertando margem e removendo erros

--- connectCar/src/App.tsx
+++ connectCar/src/App.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Header/Header";
 import Rodape from "./components/Footer/Footer";
-import { Body } from "./style/styled";
 
 export default function App() {
   return (
     <div>
-      <Body>
-        <Cabecalho />
-        <Outlet />
-        <Rodape />
-      </Body>
+      <Cabecalho />
+      <Outlet />
+      <Rodape />
     </div>
   );
 }
